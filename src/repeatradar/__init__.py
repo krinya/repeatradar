d568fddccd91c99b40ea5c874fbf0e2a50452b5a
Kristof Menyhert <menyhert.kristof@gmail.com>
2025-05-16 14:52:02 +0200

    Bump version: 0.3.3 → 0.3.4

--- src/repeatradar/__init__.py
+++ src/repeatradar/__init__.py
@@ -1,7 +1,7 @@
 # my_simple_package/my_simple_package/__init__.py
 # Make the version easily accessible (matches pyproject.toml)
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 # To keep __init__.py clean for larger projects, you could put 'greet'
 # in a separate file like 'my_simple_package/aux.py' and import it here:
