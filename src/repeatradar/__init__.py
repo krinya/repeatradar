e61b7b9b411650e3c9fb5670ba71c2a83e7f8ee0
Kristof Menyhert <menyhert.kristof@gmail.com>
2025-05-16 14:06:54 +0200

    fix: Update version number to 0.1.2 in pyproject.toml and __init__.py

--- src/repeatradar/__init__.py
+++ src/repeatradar/__init__.py
@@ -1,6 +1,6 @@
 # my_simple_package/my_simple_package/__init__.py
 # Make the version easily accessible (matches pyproject.toml)
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # To keep __init__.py clean for larger projects, you could put 'greet'
 # in a separate file like 'my_simple_package/aux.py' and import it here:
