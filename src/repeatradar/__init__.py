d59b501367328e24db9762d5c5bd5f017ef650ff
Kristof Menyhert <menyhert.kristof@gmail.com>
2025-06-01 16:20:28 +0200

    Bump version: 0.4.0 → 0.4.1

--- src/repeatradar/__init__.py
+++ src/repeatradar/__init__.py
@@ -1,7 +1,7 @@
 # repeatradar package initialization
 # Make the version easily accessible (matches pyproject.toml)
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 # Import core functionality
 from .sample import greet, create_sample_data, filter_for_name
